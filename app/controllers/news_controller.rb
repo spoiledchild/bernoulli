fad14c694ecb1ce75f5a765bb060bd712c366780
rails new rails girls <rails -v>
2014-06-29 16:15:27 +0800

    Add new columns to controllers/view/show pages

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -69,6 +69,6 @@ class NewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def news_params
-      params.require(:news).permit(:title, :date, :post)
+      params.require(:news).permit(:title, :date, :post, :TitleEn, :PostEn)
     end
 end
